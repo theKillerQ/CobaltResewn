8d02101ca1f20791cecfe14ac87a1b3d8a1adc99
Fusion1013 <william.reutmer@gmail.com>
2024-06-24 23:42:28 +0200

    Remove unused dependencies

--- src/main/java/se/fusion1013/Main.java
+++ src/main/java/se/fusion1013/Main.java
@@ -1,19 +1,17 @@
 package se.fusion1013;
 
-import dev.emi.trinkets.api.TrinketsApi;
+import net.fabricmc.api.ModInitializer;
 import net.fabricmc.fabric.api.event.lifecycle.v1.ServerLifecycleEvents;
 import net.minecraft.server.MinecraftServer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import se.fusion1013.block.CobaltBlocks;
 import se.fusion1013.block.entity.CobaltBlockEntityTypes;
 import se.fusion1013.commands.CobaltCommands;
 import se.fusion1013.effect.CobaltEffects;
 import se.fusion1013.entity.CustomEntityRegistry;
-import se.fusion1013.items.CustomItemGroupRegistry;
 import se.fusion1013.items.CobaltItems;
-import net.fabricmc.api.ModInitializer;
-
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import se.fusion1013.items.CustomItemGroupRegistry;
 import se.fusion1013.networking.CobaltServerNetworking;
 import se.fusion1013.screen.CobaltScreenHandlers;
 
