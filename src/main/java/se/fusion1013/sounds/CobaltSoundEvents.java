24c8dda1f64740f77138bfcad74e24540250d943
Fusion1013 <william.reutmer@gmail.com>
2024-07-30 21:26:33 +0200

    Add illusory wall sound

--- src/main/java/se/fusion1013/sounds/CobaltSoundEvents.java
+++ src/main/java/se/fusion1013/sounds/CobaltSoundEvents.java
@@ -14,6 +14,8 @@ public class CobaltSoundEvents {
     public static final SoundEvent LIGHT_HOLDER_LENS_INSERT = register("block.light_holder.lens_insert");
     public static final SoundEvent LIGHT_HOLDER_LENS_REMOVE = register("block.light_holder.lens_remove");
 
+    public static final SoundEvent ILLUSORY_WALL = register("illusory_wall");
+
     public static SoundEvent register(String id) {
         Identifier identifier = new Identifier(Main.MOD_NAMESPACE + ":" + id);
         return Registry.register(Registries.SOUND_EVENT, identifier, SoundEvent.of(identifier));
