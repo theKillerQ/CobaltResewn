6114ab7ecfb504689e168bc653cefec78d3f71c1
Fusion1013 <william.reutmer@gmail.com>
2024-08-07 12:20:38 +0200

    Add data generator

---
+++ src/main/java/se/fusion1013/data/CobaltDataGenerator.java
@@ -0,0 +1,11 @@
+package se.fusion1013.data;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class CobaltDataGenerator implements DataGeneratorEntrypoint {
+    @Override
+    public void onInitializeDataGenerator(FabricDataGenerator generator) {
+        FabricDataGenerator.Pack pack = generator.createPack();
+    }
+}
